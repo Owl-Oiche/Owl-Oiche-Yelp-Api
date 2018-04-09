b23a7336c42962dc2086cad4b41c9a0eddf0b97c
pkallas <pkallas29@gmail.com>
2018-04-09 14:04:23 -0700

    Added route to query yelp for details about one business

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -7,4 +7,10 @@ router.get('/yelpResults', (req, res) => {
     .catch(err => res.json(err.message));
 });
 
+router.get('/yelpIdResults', (req, res) => {
+  return yelpApi.search(req.query.id)
+    .then(response => res.json(response.data))
+    .catch(err => res.json(err.message));
+});
+
 module.exports = router;

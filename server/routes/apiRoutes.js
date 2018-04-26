2a340604c851d865041731f93efba2f73b320457
pkallas <pkallas29@gmail.com>
2018-04-26 13:50:32 -0700

    Added api route using google maps to convert latitude and longitude to city name

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const yelpApi = require('../yelp_api');
+require('dotenv').config();
+
+const GOOGLE_MAPS_KEY = process.env.GOOGLE_MAPS_KEY;
 
 router.get('/yelpResults', (req, res) => {
   return yelpApi.search(req.query)
@@ -10,6 +13,13 @@ router.get('/yelpResults', (req, res) => {
 router.get('/yelpIdResults', (req, res) => {
   return yelpApi.idSearch(req.query.id)
     .then(response => res.json(response.data))
+    .catch(err => res.json(err.message));
+});
+
+router.get('/googleMaps', (req, res) => {
+  return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=${GOOGLE_MAPS_KEY}`)
+    .then(response => response.data)
+    .then(data => res.json(data.results.formatted_address))
     .catch(err => res.json(err.message));
 });
 
